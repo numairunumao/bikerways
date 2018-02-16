d1c691c568cfb5744f256e9b7d1fb051fb87e76d
Sorasak Boontham <sorasakboontham@Sorasaks-MacBook-Pro.local>
2018-02-16 22:59:07 +0700

    Bike page

--- themes/bkw2017/single-bikes.php
+++ themes/bkw2017/single-bikes.php
@@ -1,40 +1,100 @@
 <?php get_header(); ?>
 <div class="container">
+
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<?php echo the_content(); ?>
 	<?php endwhile; else: ?>
 	<?php echo "No Content Found"; ?>
 	<?php endif; ?>
-	<h1><?php //echo $tb_brand; ?></h1>
-	<table class="table-single-bike table table-dark table-hover">
+	
+	<table class="table-single-bike table">
+		<thead>
+			<td colspan="2">ข้อมูลเบื้องต้น</td>
+		</thead>
 		<tbody>
+			<tr>
+				<td>ยี่ห้อ</td>
+				<td><?php echo do_shortcode('[lscf_customfield custom_field_id="ยี่ห้อ__pxid_lmgmbeboojyqcwt_0"]'); ?></td>
+			</tr>
 			<tr>
 				<td>ราคา</td>
 				<td><?php echo do_shortcode('[lscf_customfield custom_field_id="ราคา__pxid_zmxaxwovkxzkkzc_0"]'); ?></td>
-			</tr>
-			<tr>
-				<td>จำนวนสูบ</td>
-				<td><?php echo do_shortcode('[lscf_customfield custom_field_id="จำนวนสูบ__pxid_xsvcyjgxmsvklxw_1"]') ?></td>
-			</tr>
-			<tr>
-				<td>ระบบเกียร์</td>
-				<td><?php echo do_shortcode('[lscf_customfield custom_field_id="เกียร์__pxid_ozzkzwfywdkjptx_2"]') ?></td>
-			</tr>
-			<tr>
-				<td>แรงม้า</td>
-				<td><?php the_field('bike_horse_power'); ?></td>
-			</tr>
-			<tr>
-				<td>ABS</td>
-				<td><?php echo do_shortcode('[lscf_customfield custom_field_id="abs__pxid_glurmzxqognmlyf_3"]') ?></td>
 			</tr>
 			<tr>
 				<td>ประเภท</td>
 				<td><?php echo do_shortcode('[lscf_customfield custom_field_id="ประเภท__pxid_trhfjnsohrikukg_0"]'); ?></td>
 			</tr>
 			<tr>
+				<td>เว็ปไซค์</td>
+				<td><a target="_blank" href="<?php the_field('bike_web'); ?>"><?php the_field('bike_web'); ?></a></td>
+			</tr>
+			<tr>
+				<td>ลงข้อมูลเมื่อวันที่</td>
+				<td><?php the_field('bike_date_update'); ?></td>
+			</tr>
+			
+		</tbody>
+	</table>
+	
+	<table class="table-single-bike table">
+		<thead>
+			<tr>
+				<td colspan="2">ENGINE AND TRANSMISSION</td>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>แบบเครื่องยนตร์</td>
+				<td><?php the_field('bike_engine'); ?></td>
+			</tr>
+			<tr>
+				<td>จำนวนสูบ</td>
+				<td><?php echo do_shortcode('[lscf_customfield custom_field_id="จำนวนสูบ__pxid_xsvcyjgxmsvklxw_1"]') ?></td>
+			</tr>
+			<tr>
 				<td>กระบอกสูบ</td>
 				<td><?php echo do_shortcode('[lscf_customfield custom_field_id="cc__pxid_xptgvaigzholynp_1"]') ?></td>
+			</tr>
+			<tr>
+				<td>แรงม้า</td>
+				<td><?php the_field('bike_horse_power'); ?></td>
+			</tr>
+			
+			<tr>
+				<td>ปริมาตรกระบอกสูบ</td>
+				<td><?php the_field('bike_real_cc'); ?></td>
+			</tr>
+			<tr>
+				<td>อัตราส่วนแรงอัด</td>
+				<td><?php the_field('bike_compression_ratio'); ?></td>
+			</tr>
+			<tr>
+				<td>แรงบิดสูงสุด</td>
+				<td><?php the_field('bike_torque_power'); ?></td>
+			</tr>
+			<tr>
+				<td>ระบบจ่ายน้ำมัน</td>
+				<td><?php the_field('bike_oil_dispenser'); ?></td>
+			</tr>
+			<tr>
+				<td>ระบบสตาร์ท</td>
+				<td><?php the_field('bike_starter'); ?></td>
+			</tr>
+			<tr>
+				<td>ระบบจุดระเบิด</td>
+				<td><?php the_field('bike_Ignition_system'); ?></td>
+			</tr>
+			<tr>
+				<td>ระบบคลัช</td>
+				<td><?php the_field('bike_clutch'); ?></td>
+			</tr>
+			<tr>
+				<td>ระบบเกียร์</td>
+				<td><?php echo do_shortcode('[lscf_customfield custom_field_id="เกียร์__pxid_ozzkzwfywdkjptx_2"]') ?></td>
+			</tr>
+			<tr>
+				<td>จำนวนเกียร์</td>
+				<td><?php the_field('bike_number_gear'); ?></td>
 			</tr>
 			<tr>
 				<td>ระบบวาล์ว</td>
@@ -45,36 +105,8 @@
 				<td><?php the_field('bike_cooler'); ?></td>
 			</tr>
 			<tr>
-				<td>แบบเครื่องยนตร์</td>
-				<td><?php the_field('bike_engine'); ?></td>
-			</tr>
-			<tr>
-				<td>ระบบจ่ายน้ำมัน</td>
-				<td><?php the_field('bike_oil_dispenser'); ?></td>
-			</tr>
-			<tr>
-				<td>ระบบจุดระเบิด</td>
-				<td><?php the_field('bike_Ignition_system'); ?></td>
-			</tr>
-			<tr>
-				<td>แรงบิด</td>
-				<td><?php the_field('bike_torque_power'); ?></td>
-			</tr>
-			<tr>
-				<td>จำนวนเกียร์</td>
-				<td><?php the_field('bike_number_gear'); ?></td>
-			</tr>
-			<tr>
 				<td>ความจุถังน้ำมัน</td>
 				<td><?php the_field('bike_fuel_tank'); ?></td>
-			</tr>
-			<tr>
-				<td>ระบบสตาร์ท</td>
-				<td><?php the_field('bike_starter'); ?></td>
-			</tr>
-			<tr>
-				<td>คลัช</td>
-				<td><?php the_field('bike_clutch'); ?></td>
 			</tr>
 			<tr>
 				<td>น้ำมันเชื่อเพลิง</td>
@@ -83,6 +115,20 @@
 			<tr>
 				<td>ระบบจ่ายน้ำมัน</td>
 				<td><?php the_field('bike_oil_dispenser'); ?></td>
+			</tr>
+			
+		</tbody>
+	</table>
+	<table class="table-single-bike table">
+		<thead>
+			<tr>
+				<td colspan="2">FRAME, DIMENSION & WEIGHT</td>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>ABS</td>
+				<td><?php echo do_shortcode('[lscf_customfield custom_field_id="abs__pxid_glurmzxqognmlyf_3"]') ?></td>
 			</tr>
 			<tr>
 				<td>ระบบกันสะเทือน ล้อหน้า</td>
@@ -106,7 +152,7 @@
 				<td><?php the_field('bike_weight'); ?></td>
 			</tr>
 			<tr>
-				<td>ขนาดรถ</td>
+				<td>ขนาดรถ กว้าง X ยาว X สูง (มม.)</td>
 				<td><?php the_field('bike_dimention'); ?></td>
 			</tr>
 			<tr>
@@ -124,15 +170,7 @@
 			<tr>
 				<td>ชนิดของล้อ</td>
 				<td><?php the_field('bike_wheel_tylpe'); ?></td>
-			</tr>
-			<tr>
-				<td>เว็ปไซค์</td>
-				<td><a target="_blank" href="<?php the_field('bike_web'); ?>"><?php the_field('bike_web'); ?></a></td>
-			</tr>
-			<tr>
-				<td>ลงข้อมูลเมื่อวันที่</td>
-				<td><?php the_field('bike_date_update'); ?></td>
-			</tr>
+			</tr>		
 		</tbody>
 	</table>
 </div>
